20e298676ab3099393f9aa325778293be8e676c7
lony2003 <zhangke200377@outlook.com>
2024-02-12 19:06:52 +0800

    fix cloudflare captcha can not display error

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -18,11 +18,9 @@ let tab = ref("0")
     </div>
   </div>
   <iframe :hidden="tab != '0'" class="underside-iframe" :class="{ active: tab == '0' }" name="underside-iframe-container" frameborder="0"
-          csp="frame-src https://www.bing.com/search https://edgeservices.bing.com/ https://www.bing.com/shop/productpage https://www.bing.com/images/create https://login.live.com/login.srf https://www.bing.com/turing/captcha/challenge https://www.bing.com/api/shopping/v1/edgeframe https://www.msn.com/widgets/fullpage/gaming/gamingfeed-widget https://challenges.cloudflare.com/ https://www.bing.com/lite/auw https://www.bing.com/travelgrowthedge/ux https://login.live.com/ https://login.microsoftonline.com/; base-uri 'self'; require-trusted-types-for 'script'; trusted-types default;"
           allow="clipboard-write;microphone;camera" :src="chatUrl"
           style="position: absolute; width: 100%; height: calc(100% - 37px);; top: 0; left: 0; padding-top: 37px"></iframe>
   <iframe :hidden="tab != '1'" class="underside-iframe" :class="{ active: tab == '1' }" name="underside-iframe-container" frameborder="0"
-          csp="frame-src https://www.bing.com/search https://edgeservices.bing.com/ https://www.bing.com/shop/productpage https://www.bing.com/images/create https://login.live.com/login.srf https://www.bing.com/turing/captcha/challenge https://www.bing.com/api/shopping/v1/edgeframe https://www.msn.com/widgets/fullpage/gaming/gamingfeed-widget https://challenges.cloudflare.com/ https://www.bing.com/lite/auw https://www.bing.com/travelgrowthedge/ux https://login.live.com/ https://login.microsoftonline.com/; base-uri 'self'; require-trusted-types-for 'script'; trusted-types default;"
           allow="clipboard-write;microphone;camera" :src="composeUrl"
           style="position: absolute; width: 100%; height: calc(100% - 37px); top: 0; left: 0; padding-top: 37px"></iframe>
 </template>
